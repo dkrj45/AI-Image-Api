91ee521af006f08080de40ae76da6f151beef591
Chris Lee <dkrj45@gmail.com>
2022-08-31 20:57:26 -0400

    api is now functional. additional images required

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const app = express()
 const cors = require("cors");
-const videosRoutes =  require("./routes/videosRoutes")
+const imagesRoutes =  require("./routes/imagesRoutes")
+const profilesRoutes = require("./routes/profilesRoutes")
 require('dotenv').config()
 const {PORT} = process.env
+
+app.use(cors());
+
+// use this if we need access to req.body
+// app.use(express.json());
+
+app.use('/static',express.static('public'))
+
+app.use("/images", imagesRoutes);
+
+app.use("/profiles", profilesRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server is ready on port ${PORT}`)
+})
